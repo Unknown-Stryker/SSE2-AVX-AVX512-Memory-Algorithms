a4037db35d58a6d5b79c12b27d8414dbe327b728
Unknown-Stryker <122397312+Unknown-Stryker@users.noreply.github.com>
2024-11-06 15:19:48 +0900

    Update __x86_64_memcpy.h

--- __x86_64_memcpy.h
+++ __x86_64_memcpy.h
@@ -2,8 +2,21 @@
 #define __X86_64_MEMCPY_H_
 #include <cstddef>
 /*
-Copyright © from 2023 to current, UNKNOWN STRYKER. All Rights Reserved.
+Copyright © from 2022 to present, UNKNOWN STRYKER. All Rights Reserved.
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+/*
 It was written in AT&T assembly to outperform and replace the C++ implementation of FE::memcpy.
 Sadly, it could not run fater than the optimized C++ code with clang++-12 -O3.
 It was worth spending time and efforts to learn how C functions work on x86-64 CPU architecture.
